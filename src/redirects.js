d85b613e186397d2bc8684693932f3cb239a250b
RichyHBM <gitemail@richyhbm.co.uk>
2026-04-01 17:49:21 +0100

    Try redirect

---
+++ src/redirects.js
@@ -0,0 +1,5 @@
+export const APP_REDIRECTS = {
+    '/landing/team': '/',
+    '/landing/contact': '/',
+    '/news/news': '/'
+}
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { APP_REDIRECTS } from './src/redirects.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,6 +9,12 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		prerender: {
+			entries: [
+				'*',
+				...Object.keys(APP_REDIRECTS)
+			]
+		},
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
